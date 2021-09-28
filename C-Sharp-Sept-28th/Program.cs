4ff35182f9b1598ae2ce76230b1390b4b37cdf0c
Lashonda Miller <lashondamiller007@gmail.com>
2021-09-28 13:48:40 -0400

    Topic class

--- C-Sharp-Sept-28th/Program.cs
+++ C-Sharp-Sept-28th/Program.cs
@@ -6,7 +6,23 @@ namespace C_Sharp_Sept_28th
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // BootCamp Corriculum
+
+            var gitgithub = new Topic();
+            //gitgithub.Id = 400;
+            gitgithub.Name = "Git/GitHub";
+            gitgithub.Category = "SCM"; // Source Control Management
+            gitgithub.Difficulty = 2; // level 1 - 3 (easy - hard)
+            gitgithub.print();
+
+            var sqlserver = new Topic()
+            {
+                Name = "SQL Server",
+                Category = "Database",
+                Difficulty = 3
+            };
+            sqlserver.print();
+
         }
     }
 }
---
+++ C-Sharp-Sept-28th/Topic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Sept_28th
+{
+    class Topic
+    {
+        private static int NextId { get; set; } = 11;
+
+        public int Id { get; private set; } // private set means program class and no other class can change the Id
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Difficulty { get; set; }
+
+        //public void SetId(int id)
+        //{
+        //    this.Id = id;
+        //}
+
+        public void print() {
+            Console.WriteLine($"{Id} | {Name} | {Category} | {Difficulty}"); // use console.writline when print is set to void
+        }
+
+        public Topic()
+        {
+            this.Id = NextId;
+            NextId += 20;
+        }
+    }
+}
