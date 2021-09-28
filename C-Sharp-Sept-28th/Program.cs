64e2209e075cc60ac48ff19df0eeb4c189693196
Lashonda Miller <lashondamiller007@gmail.com>
2021-09-28 09:14:37 -0400

    initial commit

---
+++ C-Sharp-Sept-28th/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Sept_28th
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
